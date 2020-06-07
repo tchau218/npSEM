fccefbeeecfc7896ffcb5a0d0e4523b13bc51eb3
trang-chau <32773100+trang-chau@users.noreply.github.com>
2020-06-07 15:46:11 +0200

    Delete save_load.py

--- save_load.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-def saveTr(file_name, data):
-    with open(file_name, 'wb') as f:
-        pickle.dump(data, f)
-            
-def loadTr(file_name):
-    with open(file_name, 'rb') as pickle_file:
-        return pickle.load(pickle_file)
